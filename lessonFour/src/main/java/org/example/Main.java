6993d186f341036a7752a659c41d96120be9b274
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-08-20 23:40:01 +0500

    ...

---
+++ lessonFour/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        SimpleBean simpleBean = context.getBean(SimpleBean.class);
+
+
+        ((AbstractApplicationContext) context).getBeanFactory().destroyBean("simpleBean", simpleBean);
+    }
+}
