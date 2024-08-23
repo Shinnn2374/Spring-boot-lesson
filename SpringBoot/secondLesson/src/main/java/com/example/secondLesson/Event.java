c38c78bd043490845d1a6ff36ce700a5a7eaa6cf
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-08-23 11:26:08 +0500

    ...

---
+++ SpringBoot/secondLesson/src/main/java/com/example/secondLesson/Event.java
@@ -0,0 +1,19 @@
+package com.example.secondLesson;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Event
+{
+    private UUID id;
+    private String pilot;
+
+}
---
+++ SpringBoot/secondLesson/src/test/java/com/example/secondLesson/SecondLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secondLesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
