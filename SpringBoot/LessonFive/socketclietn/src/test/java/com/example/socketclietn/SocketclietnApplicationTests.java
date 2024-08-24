83f1b31ff76875fe51fe87076e8fbcf548c7500e
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-08-24 16:25:54 +0500

    ...

---
+++ SpringBoot/LessonFive/socketclietn/src/test/java/com/example/socketclietn/SocketclietnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socketclietn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketclietnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
