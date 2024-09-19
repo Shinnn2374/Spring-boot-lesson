8435e7335c0000c7421d7bebf84053bfb6d4f292
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-09-19 11:32:00 +0300

    ...

--- RestAPIwithDB/rest/src/main/java/com/example/rest/web/model/UpsertOrderRequest.java
+++ RestAPIwithDB/rest/src/main/java/com/example/rest/web/model/UpsertOrderRequest.java
@@ -1,5 +1,8 @@
 package com.example.rest.web.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -7,7 +10,14 @@ import java.math.BigDecimal;
 @Data
 public class UpsertOrderRequest
 {
+    @NotNull(message = "ID клиента должно быть указано")
+    @Positive(message = "ID клиента должно быть больше 0")
     private Long clientId;
+
+    @NotBlank(message = "Имя продукта должно быть указано")
     private String product;
+
+    @NotNull(message = "Цена продукта должна быть указанна")
+    @Positive(message = "Цена продукта должна быть больше 0")
     private BigDecimal cost;
 }
