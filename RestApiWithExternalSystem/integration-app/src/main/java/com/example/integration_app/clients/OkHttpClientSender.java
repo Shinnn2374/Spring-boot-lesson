8e85895fd9d0daadea2cef1acac773fb72897c4e
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-10-16 17:44:16 +0300

    ...

--- RestApiWithExternalSystem/integration-app/src/main/java/com/example/integration_app/clients/OkHttpClientSender.java
+++ RestApiWithExternalSystem/integration-app/src/main/java/com/example/integration_app/clients/OkHttpClientSender.java
@@ -1,5 +1,7 @@
 package com.example.integration_app.clients;
 
+import com.example.integration_app.model.EntityModel;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -50,7 +53,6 @@ public class OkHttpClientSender
         }
     }
 
-    @SneakyThrows
     public Resource downloadFile(String fileName)
     {
        Request request = new Request.Builder()
@@ -71,5 +73,43 @@ public class OkHttpClientSender
            e.printStackTrace();
            return null;
        }
+    }
+
+    public List<EntityModel> getEntityList()
+    {
+        Request request = new Request.Builder()
+                .url(baseUrl + "/api/v1/entity")
+                .build();
+        return processResponse(request,new TypeReference<>(){});
+    }
+    public EntityModel getEntityByName(String name)
+    {
+        Request request = new Request.Builder()
+                .url(baseUrl + "api/v1/entity/" + name)
+                .build();
+        return processResponse(request, new TypeReference<>(){});
+    }
+
+
+    @SneakyThrows
+    public <T> T processResponse(Request request, TypeReference<T> typeReference)
+    {
+        try(Response response = client.newCall(request).execute())
+        {
+            if (!response.isSuccessful())
+            {
+                throw new RuntimeException("Unexpected response code: " + response);
+            }
+            ResponseBody body = response.body();
+            if (body != null)
+            {
+                String stringBody = body.string();
+                return objectMapper.readValue(stringBody, typeReference);
+            }
+            else
+            {
+                throw new RuntimeException("Response body is empty");
+            }
+        }
     }
 }
