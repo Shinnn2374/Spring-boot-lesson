8a156efff4151e5d8b798fab702e6f9fbd722710
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-10-16 14:46:09 +0300

    ...

--- RestApiWithExternalSystem/integration/src/main/java/com/example/integration/controller/EntityController.java
+++ RestApiWithExternalSystem/integration/src/main/java/com/example/integration/controller/EntityController.java
@@ -7,8 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/entity")
@@ -36,5 +38,18 @@ public class EntityController
     public ResponseEntity<EntityModel> createEntity(@RequestBody UpsertEntityRequest request)
     {
         return ResponseEntity.status(HttpStatus.CREATED).body(EntityModel.createMockModel(request.getName()));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EntityModel> updateEntity(@PathVariable UUID id, @RequestBody UpsertEntityRequest request)
+    {
+        return ResponseEntity.ok(new EntityModel(id,request.getName(), Instant.now()));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMapping(@PathVariable UUID id)
+    {
+        log.info("Delete entity by id {}", id);
+        return ResponseEntity.noContent().build();
     }
 }
