38f53b3f0c66910b3b33170f722365084419f65f
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-10-27 00:29:35 +0300

    ...

--- RestApiWithExternalSystem/integration-app/src/main/java/com/example/integration_app/clients/OkhttpClientSender.java
+++ RestApiWithExternalSystem/integration-app/src/main/java/com/example/integration_app/clients/OkhttpClientSender.java
@@ -1,13 +1,43 @@
 package com.example.integration_app.clients;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class OkhttpClientSender
 {
-    
+    private OkHttpClient client;
+    private ObjectMapper objectMapper;
+    @Value("${app.integration.base-url}")
+    private String baseUrl;
+
+    @SneakyThrows
+    public String loadFile(MultipartFile file)
+    {
+        MultipartBody.Builder builder = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("file", file.getName(),
+                        RequestBody.create(MediaType.parse("application/octet-stream"), file.getBytes()));
+        Request request = new Request.Builder()
+                .url(baseUrl + "/api/v1/file/upload")
+                .header("Content-Type", "multipart/from-data")
+                .post(builder.build())
+                .build();
+        try(Response response = client.newCall(request).execute())
+        {
+            if (!response.isSuccessful())
+            {
+                log.error("Error trying to request")
+            }
+        }
+
+    }
 }
