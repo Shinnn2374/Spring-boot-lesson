d284b04cc97c44f715a5e8a96ddc9d413f2e2ff2
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-10-27 16:23:02 +0300

    ...

---
+++ RestApiWithExternalSystem/integration-app/src/main/java/com/example/integration_app/controller/FileClientController.java
@@ -0,0 +1,36 @@
+package com.example.integration_app.controller;
+
+import com.example.integration_app.clients.OkhttpClientSender;
+import lombok.RequiredArgsConstructor;
+import okhttp3.OkHttpClient;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/v1/client/file")
+@RequiredArgsConstructor
+public class FileClientController
+{
+    private final OkhttpClientSender client;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestPart MultipartFile file)
+    {
+        client.uploadFile(file);
+        return ResponseEntity.ok("File was upload");
+    }
+
+    @GetMapping("/download/{filename}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename)
+    {
+        Resource resource = client.downLoadFile(filename);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attachment; filename=" + filename);
+        headers.setContentType(MediaType.TEXT_PLAIN);
+        return ResponseEntity.ok().headers(headers).body(resource);
+    }
+}
