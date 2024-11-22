c13992861fad6495719a270c199febe319b69d17
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-11-23 02:50:08 +0300

    ....

--- RestApiWithExternalSystem/integration-app/src/main/java/com/example/integration_app/service/DataBaseEntityService.java
+++ RestApiWithExternalSystem/integration-app/src/main/java/com/example/integration_app/service/DataBaseEntityService.java
@@ -3,6 +3,7 @@ package com.example.integration_app.service;
 import com.example.integration_app.entity.DataBaseEntity;
 import com.example.integration_app.repository.DataBaseEntityRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
@@ -15,6 +16,7 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@CacheConfig(cacheManager = "redisCacheManager")
 public class DataBaseEntityService
 {
     private final DataBaseEntityRepository dataBaseEntityRepository;
