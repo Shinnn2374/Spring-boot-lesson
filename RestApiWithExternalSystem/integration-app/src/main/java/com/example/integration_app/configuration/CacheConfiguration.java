cfd0d96ea5da872ffdc6be9490b4c1a54623b28e
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-11-22 17:19:35 +0300

    ...

--- RestApiWithExternalSystem/integration-app/src/main/java/com/example/integration_app/configuration/CacheConfiguration.java
+++ RestApiWithExternalSystem/integration-app/src/main/java/com/example/integration_app/configuration/CacheConfiguration.java
@@ -3,13 +3,21 @@ package com.example.integration_app.configuration;
 import com.example.integration_app.configuration.properties.AppCacheProperties;
 import com.google.common.cache.CacheBuilder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableCaching
@@ -36,10 +44,31 @@ public class CacheConfiguration
             }
         };
         var cacheNames = appCacheProperties.getCacheNames();
-        if (cacheNames != null)
+        if (!cacheNames.isEmpty())
         {
             cacheManager.setCacheNames(cacheNames);
         }
         return cacheManager;
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "app.redis",name = "enable", havingValue = "true")
+    @ConditionalOnExpression("'${app.cache.cacheType}'.equals('REDIS')")
+    public CacheManager redisCacheManager(AppCacheProperties appCacheProperties,
+                                          LettuceConnectionFactory lettuceConnectionFactory)
+    {
+        var defaultConfig = RedisCacheConfiguration.defaultCacheConfig();
+        Map<String, RedisCacheConfiguration> redisCacheConfiguration = new HashMap<>();
+        appCacheProperties.getCacheNames().forEach(
+                name -> {
+                    redisCacheConfiguration.put(name, RedisCacheConfiguration.defaultCacheConfig().entryTtl(
+                            appCacheProperties.getCaches().get(name).getExpire()
+                    ));
+                }
+        );
+        return RedisCacheManager.builder(lettuceConnectionFactory)
+                .cacheDefaults(defaultConfig)
+                .withInitialCacheConfigurations(redisCacheConfiguration)
+                .build();
     }
 }
