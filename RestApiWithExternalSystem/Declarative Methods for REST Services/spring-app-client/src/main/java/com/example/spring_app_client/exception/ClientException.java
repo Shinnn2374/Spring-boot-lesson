be2a9f35956e703316d000c024974405628a6706
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-12-23 22:19:55 +0300

    ...

--- RestApiWithExternalSystem/Declarative Methods for REST Services/spring-app-client/src/main/java/com/example/spring_app_client/exception/ClientException.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring_app_client.exception;
-
-public class ClientException extends RuntimeException {
-  public ClientException(String message) {
-    super(message);
-  }
-}
--- RestApiWithExternalSystem/integration-app/src/main/java/com/example/integration_app/model/UpsertEntityRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.integration_app.model;
-
-import lombok.Data;
-
-@Data
-public class UpsertEntityRequest
-{
-    private String name;
-}
