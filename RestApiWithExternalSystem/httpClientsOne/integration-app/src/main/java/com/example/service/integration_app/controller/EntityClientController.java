f2c5b4668f0b9fbab23dc802eabe10b4273bb410
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-01-04 01:32:09 +0500

    ...

--- RestApiWithExternalSystem/httpClientsOne/integration-app/src/main/java/com/example/service/integration_app/controller/EntityClientController.java
+++ RestApiWithExternalSystem/httpClientsOne/integration-app/src/main/java/com/example/service/integration_app/controller/EntityClientController.java
@@ -1,9 +1,15 @@
 package com.example.service.integration_app.controller;
 
 import com.example.service.integration_app.clients.OkHttpClientSender;
+import com.example.service.integration_app.model.EntityModel;
+import com.example.service.integration_app.model.UpsertEntityRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/client/entity")
@@ -12,5 +18,34 @@ public class EntityClientController
 {
     private final OkHttpClientSender client;
 
-    
+    @GetMapping
+    public ResponseEntity<List<EntityModel>> getEntityList()
+    {
+        return ResponseEntity.ok(client.getEntityList());
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<EntityModel> getEntityByName(@PathVariable String name)
+    {
+        return ResponseEntity.ok(client.getEntityByName(name));
+    }
+
+    @PostMapping
+    public ResponseEntity<EntityModel> createEntity(@RequestBody UpsertEntityRequest request)
+    {
+        return ResponseEntity.status(HttpStatus.CREATED).body(client.createEntity(request));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EntityModel> updateEntity(@PathVariable UUID id, @RequestBody UpsertEntityRequest request)
+    {
+        return ResponseEntity.ok(client.updateEntity(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<EntityModel> deleteEntity(@PathVariable UUID id)
+    {
+        client.deleteEntityById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
