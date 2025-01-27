16d44c4846a995ad3e48d6d0be503eafe94f2fc8
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-01-27 18:22:53 +0300

    ...

---
+++ RestApiWithExternalSystem/httpClientsOne/integration-app/src/main/java/com/example/integration_app/clients/WebClientSender.java
@@ -0,0 +1,68 @@
+package com.example.integration_app.clients;
+
+import com.example.integration_app.model.EntityModel;
+import lombok.RequiredArgsConstructor;
+import okhttp3.MultipartBody;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.BodyInserter;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class WebClientSender
+{
+    public final WebClient webClient;
+
+    public void uploadFile(MultipartFile file)
+    {
+        MultipartBodyBuilder builder = new MultipartBodyBuilder();
+        builder.part("file",file.getResource())
+                .filename(file.getName())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM);
+        webClient.post()
+                .uri("/api/v1/file/upload")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE)
+                .body(BodyInserters.fromMultipartData(builder.build()))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+
+    public Resource downloadFile(String fileName)
+    {
+        return webClient.get()
+                .uri("/api/v1/file/download/{filename}",fileName)
+                .accept(MediaType.APPLICATION_OCTET_STREAM)
+                .retrieve()
+                .bodyToMono(Resource.class)
+                .block();
+    }
+
+    public List<EntityModel> getEntityList()
+    {
+        return webClient.get()
+                .uri("/api/v1/client//entity")
+                .retrieve()
+                .bodyToFlux(EntityModel.class)
+                .collectList()
+                .block();
+    }
+
+    public EntityModel getEntityByName(String entityName){
+        return webClient.get()
+                .uri("/api/v1/entity/{entityName}",entityName)
+                .retrieve()
+                .bodyToMono(EntityModel.class)
+                .block();
+    }
+
+    public EntityModel
+}
