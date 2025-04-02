89e97ada97f22fea974b244357707cc9e05ac26e
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-04-02 19:26:07 +0300

    ...

--- SpringSecurity/spring-basic-auth-example/src/main/java/com/example/spring_basic_auth_example/controller/PublicController.java
+++ SpringSecurity/spring-basic-auth-example/src/main/java/com/example/spring_basic_auth_example/controller/PublicController.java
@@ -1,17 +1,40 @@
 package com.example.spring_basic_auth_example.controller;
 
+import com.example.spring_basic_auth_example.entity.Role;
+import com.example.spring_basic_auth_example.entity.RoleType;
+import com.example.spring_basic_auth_example.entity.User;
+import com.example.spring_basic_auth_example.model.UserDto;
+import com.example.spring_basic_auth_example.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/public")
+@RequiredArgsConstructor
 public class PublicController {
+
+    private final UserService userService;
 
     @GetMapping
     public ResponseEntity<String> getPublic() {
         return ResponseEntity.ok("call public method");
+    }
+
+    @PostMapping("/account")
+    public ResponseEntity<UserDto> createAccount(@RequestBody UserDto userDto, @RequestParam RoleType roleType) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(createAccount(userDto,roleType));
+    }
+
+    public UserDto createAccount(UserDto userDto, RoleType roleType){
+        var user = new User();
+        user.setUsername(userDto.getUsername());
+        user.setUsername(userDto.getUsername());
+
+        var createUser = userService.createNewAccount(user, Role.from(roleType));
+        
     }
 
 }
