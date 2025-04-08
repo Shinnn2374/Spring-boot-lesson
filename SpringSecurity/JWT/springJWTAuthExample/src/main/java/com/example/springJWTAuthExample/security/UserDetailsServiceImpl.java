a15a2fa2eb8bd261250427ca79bd40c8fed4dea8
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-04-08 20:20:52 +0300

    ...

--- SpringSecurity/JWT/springJWTAuthExample/src/main/java/com/example/springJWTAuthExample/security/UserDetailsServiceImpl.java
+++ SpringSecurity/JWT/springJWTAuthExample/src/main/java/com/example/springJWTAuthExample/security/UserDetailsServiceImpl.java
@@ -1,4 +1,27 @@
 package com.example.springJWTAuthExample.security;
 
-public class UserDetailsServiceImpl {
+import com.example.springJWTAuthExample.entite.User;
+import com.example.springJWTAuthExample.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.text.MessageFormat;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        MessageFormat.format("User with name - {0} not found", username)
+                ));
+        return new AppUserDetails(user);
+    }
 }
