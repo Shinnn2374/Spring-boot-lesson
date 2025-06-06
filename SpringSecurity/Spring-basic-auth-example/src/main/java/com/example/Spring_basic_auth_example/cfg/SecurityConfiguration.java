35fe8fc8128aeeab5ac0436174340d5bbde64dd3
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-06 22:39:59 +0300

    ...

---
+++ SpringSecurity/Spring-basic-auth-example/src/main/java/com/example/Spring_basic_auth_example/cfg/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.example.Spring_basic_auth_example.cfg;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    @Bean
+    @ConditionalOnProperty(prefix = "app.security", name = "type", havingValue = "inMemory")
+    public PasswordEncoder inMemoryPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "app.security", name = "type", havingValue = "inMemory")
+    public UserDetailsService inMemoryUserDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user")
+                .password("user")
+                .roles("USER")
+                .build());
+        manager.createUser(User.withUsername("admin")
+                .password("admin")
+                .roles("USER", "ADMIN")
+                .build());
+        return manager;
+    }
+}
