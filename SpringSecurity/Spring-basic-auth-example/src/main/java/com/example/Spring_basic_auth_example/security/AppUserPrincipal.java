d880d0f39a64b4ba00b76c3421132dd72d201d9d
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-08 12:36:57 +0300

    ...

---
+++ SpringSecurity/Spring-basic-auth-example/src/main/java/com/example/Spring_basic_auth_example/security/AppUserPrincipal.java
@@ -0,0 +1,50 @@
+package com.example.Spring_basic_auth_example.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class AppUserPrincipal implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
