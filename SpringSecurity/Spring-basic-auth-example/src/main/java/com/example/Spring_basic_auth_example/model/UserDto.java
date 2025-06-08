a41b713a0b37fbe5f9ee7c154ea7be35a1e50e54
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-08 13:04:47 +0300

    ...

---
+++ SpringSecurity/Spring-basic-auth-example/src/main/java/com/example/Spring_basic_auth_example/model/UserDto.java
@@ -0,0 +1,15 @@
+package com.example.Spring_basic_auth_example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private String username;
+    private String password;
+}
