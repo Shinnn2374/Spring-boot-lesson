fb9122e11e15c77d61c5a0fc94504c7fb82a770a
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-12 18:16:56 +0300

    ...

--- SpringSecurity/JWT-auth-example/src/main/java/com/example/JWT_auth_example/web/model/LoginRequest.java
+++ SpringSecurity/JWT-auth-example/src/main/java/com/example/JWT_auth_example/web/model/LoginRequest.java
@@ -1,20 +1,14 @@
 package com.example.JWT_auth_example.web.model;
 
-import com.example.JWT_auth_example.entity.RoleType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Set;
-
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-@Builder
-public class CreateUserRequest {
+@NoArgsConstructor
+public class LoginRequest {
     private String username;
-    private String email;
-    private Set<RoleType> roles;
     private String password;
 }
---
+++ SpringSecurity/JWT-auth-example/src/main/java/com/example/JWT_auth_example/web/model/SimpleRequest.java
@@ -0,0 +1,13 @@
+package com.example.JWT_auth_example.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SimpleRequest {
+
+    private String message;
+}
