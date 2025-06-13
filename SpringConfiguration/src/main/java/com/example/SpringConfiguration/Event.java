29d55faaef93bd6cbc8189f78826535eca0dfdf5
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-13 18:19:17 +0300

    ...

--- SpringConfiguration/src/main/java/com/example/SpringConfiguration/Event.java
+++ SpringConfiguration/src/main/java/com/example/SpringConfiguration/Event.java
@@ -1,7 +1,16 @@
 package com.example.SpringConfiguration;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Event {
     private UUID   id;
     private String payload;
---
+++ SpringConfiguration/src/main/java/com/example/SpringConfiguration/EventQueueWorker.java
@@ -0,0 +1,46 @@
+package com.example.SpringConfiguration;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class EventQueueWorker {
+
+    private final EventQueue eventQueue;
+
+    public void startEventQueue() {
+        startEventProducer();
+        startEventConsumer();
+    }
+
+    private void startEventConsumer() {
+        Thread eventConsumerThread = new Thread(() -> {
+            while (true) {
+                Event event = eventQueue.dequeue();
+                System.out.println(event);
+            }
+        });
+        eventConsumerThread.start();
+    }
+
+    private void startEventProducer() {
+        Thread eventProducer = new Thread(() -> {
+            while (true) {
+                UUID id = UUID.randomUUID();
+                Event event =  Event.builder()
+                        .id(id)
+                        .payload("payload for event - " + id)
+                        .build();
+
+                eventQueue.enqueue(event);
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        eventProducer.start();
+    }
+}
