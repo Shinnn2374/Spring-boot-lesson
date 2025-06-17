5e7ca2e5f6f03f523a9e1704f42634194c1b77b2
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-17 17:05:47 +0300

    ...

--- simple-web/src/main/java/com/example/simple_web/TaskController.java
+++ simple-web/src/main/java/com/example/simple_web/TaskController.java
@@ -3,6 +3,9 @@ package com.example.simple_web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,9 +16,35 @@ public class TaskController {
     private final List<Task> tasks = new ArrayList<>();
 
     @GetMapping("/")
-    private String index(Model model) {
+    public String index(Model model) {
         model.addAttribute("tasks", tasks);
         return "index";
     }
 
+    @GetMapping("/task/create")
+    public String showcreateForm(Model model) {
+        model.addAttribute("task", new Task());
+        return "create";
+    }
+
+    @PostMapping("/task/create")
+    public String createTask(@ModelAttribute Task task) {
+        task.setId(System.currentTimeMillis());
+        tasks.add(task);
+        return "redirect:/";
+    }
+
+    @GetMapping("/task/edit/{id}")
+    public String showEditForm(@PathVariable("id") long id, Model model) {
+        Task task = findTaskById(id);
+        if (task != null) {
+            model.addAttribute("task", task);
+            return "edit";
+        }
+        return "redirect:/";
+    }
+
+    private Task findTaskById(long id) {
+        return tasks.stream().filter(task -> task.getId() == id).findFirst().orElse(null);
+    }
 }
