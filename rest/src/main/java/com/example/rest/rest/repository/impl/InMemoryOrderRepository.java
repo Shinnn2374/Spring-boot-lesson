9ecf43d0907b97f3dbfc50c04f7203b54424464e
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-18 16:28:26 +0300

    ...

--- rest/src/main/java/com/example/rest/rest/repository/impl/InMemoryOrderRepository.java
+++ rest/src/main/java/com/example/rest/rest/repository/impl/InMemoryOrderRepository.java
@@ -1,12 +1,52 @@
 package com.example.rest.rest.repository.impl;
 
+import com.example.rest.rest.model.Order;
 import com.example.rest.rest.repository.ClientRepository;
+import com.example.rest.rest.repository.OrderRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public class InMemoryOrderRepository {
+public class InMemoryOrderRepository implements OrderRepository {
 
     private ClientRepository clientRepository;
 
+    public void setClientRepository(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
     
+
+    @Override
+    public List<Order> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<Order> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Order save(Order order) {
+        return null;
+    }
+
+    @Override
+    public Order update(Order order) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+
+    @Override
+    public void deleteByIdIn(List<Long> ids) {
+
+    }
+
 }
