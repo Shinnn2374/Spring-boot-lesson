862fcb0277fca9f45ec478695e70e9a356ef6a35
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-22 19:52:55 +0300

    ...

---
+++ Spring-Security/src/main/java/com/example/Spring_Security/SecurityConfiguration.java
@@ -0,0 +1,77 @@
+package com.example.Spring_Security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    @Bean
+    @ConditionalOnProperty(prefix = "app.security", name = "type", havingValue = "InMemory")
+    public PasswordEncoder inMemoryPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "app.security", name = "type", havingValue = "InMemory")
+    public UserDetailsService inMemoryUserDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user")
+                .password("user")
+                .roles("USER")
+                .build());
+
+        manager.createUser(User.withUsername("user")
+                .password("admin")
+                .roles("ADMIN")
+                .build());
+        return manager;
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "app.security", name = "type", havingValue = "InMemory")
+    public AuthenticationManager inMemoryAuthenticationManager(HttpSecurity http,
+                                                               UserDetailsService inMemoryUserDetailsService) throws Exception {
+        var authenticationManager = http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        authenticationManager.userDetailsService(inMemoryUserDetailsService);
+
+        return authenticationManager.build();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+        http.authorizeHttpRequests((auth) -> auth.requestMatchers("/api/v1/user/**")
+                .hasAnyRole("USER", "ADMIN")
+                .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                .requestMatchers("/api/v1/public/**").permitAll()
+                .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationManager(authenticationManager);
+        return http.build();
+    }
+
+
+}
