664471f645f6de9253f29a3064a1e7fa3916aff9
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-24 14:34:55 +0300

    ...

---
+++ jet-auth-example/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "jet-auth-example"
---
+++ jet-auth-example/src/main/java/com/example/jwt_auth_example/JwtAuthExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_auth_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthExampleApplication.class, args);
+	}
+
+}
