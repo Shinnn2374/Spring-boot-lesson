1fa0f852f8011d4c7850fc830c06640a848166df
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-24 15:09:12 +0300

    ....

---
+++ jet-auth-example/src/main/java/com/example/jwt_auth_example/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.jwt_auth_example.security;
+
+import com.example.jwt_auth_example.entity.User;
+import com.example.jwt_auth_example.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.text.MessageFormat;
+
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername( username)
+                .orElseThrow(() -> new UsernameNotFoundException(MessageFormat.format("User with name: {0} not found", username)));
+        return new AppUserDetails(user);
+    }
+}
