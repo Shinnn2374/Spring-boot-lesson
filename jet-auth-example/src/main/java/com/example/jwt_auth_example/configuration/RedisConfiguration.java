98c9938d53ee0ef2cad27d859475c7b5d732a701
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-24 21:54:31 +0300

    ...

---
+++ jet-auth-example/src/main/java/com/example/jwt_auth_example/configuration/RedisConfiguration.java
@@ -0,0 +1,43 @@
+package com.example.jwt_auth_example.configuration;
+
+import com.example.jwt_auth_example.entity.RefreshToken;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.convert.KeyspaceConfiguration;
+
+import java.time.Duration;
+import java.util.Collections;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Value("${app.jwt.refreshTokenExpiration}")
+    private Duration refreshTokenExpiration;
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory(RedisProperties redisProperties) {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+
+        configuration.setHostName(redisProperties.getHost());
+        configuration.setPort(redisProperties.getPort());
+
+        return new JedisConnectionFactory(configuration);
+    }
+
+    public class RefreshTokenKeySpaceConfiguration extends KeyspaceConfiguration {
+
+        private static final String REFRESH_TOKEN_KEYSPACE = "refresh_tokens";
+
+        @Override
+        protected Iterable<KeyspaceSettings> initialConfiguration() {
+            KeyspaceSettings keyspaceSettings = new KeyspaceSettings(RefreshToken.class, REFRESH_TOKEN_KEYSPACE);
+
+            keyspaceSettings.setTimeToLive(refreshTokenExpiration.getSeconds());
+            return Collections.singleton(keyspaceSettings);
+        }
+    }
+}
