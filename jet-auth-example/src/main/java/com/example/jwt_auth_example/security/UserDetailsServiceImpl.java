abab0efea32446e60d2365bb8d913a0810da3b87
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-24 21:36:47 +0300

    ...

--- jet-auth-example/src/main/java/com/example/jwt_auth_example/security/UserDetailsServiceImpl.java
+++ jet-auth-example/src/main/java/com/example/jwt_auth_example/security/UserDetailsServiceImpl.java
@@ -6,10 +6,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.text.MessageFormat;
 
 @RequiredArgsConstructor
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
