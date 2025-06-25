bc25306309b02ed929126b4622bede72ed6fa08b
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-25 14:54:13 +0300

    ...

--- jet-auth-example/src/main/java/com/example/jwt_auth_example/web/handler/WebAppExceptionHandler.java
+++ jet-auth-example/src/main/java/com/example/jwt_auth_example/web/handler/WebAppExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.example.jwt_auth_example.web.handler;
 
+import com.example.jwt_auth_example.excaption.AlreadyExistException;
+import com.example.jwt_auth_example.excaption.EntityNotFoundException;
 import com.example.jwt_auth_example.excaption.RefreshTokenException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,6 +14,24 @@ public class WebAppExceptionHandler {
 
     @ExceptionHandler(value = RefreshTokenException.class)
     public ResponseEntity<ErrorResponseBody> refreshTokenExceptionHandler(RefreshTokenException e, WebRequest request) {
-        
+        return buildResponse(HttpStatus.FORBIDDEN, e, request);
+    }
+
+    @ExceptionHandler(value = AlreadyExistException.class)
+    public ResponseEntity<ErrorResponseBody> alreadyExistExceptionHandler(AlreadyExistException e, WebRequest request) {
+        return buildResponse(HttpStatus.BAD_REQUEST, e, request);
+    }
+
+    @ExceptionHandler(value = EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponseBody> entityNotFoundExceptionHandler(EntityNotFoundException e, WebRequest request) {
+        return buildResponse(HttpStatus.NOT_FOUND, e, request);
+    }
+
+    private ResponseEntity<ErrorResponseBody> buildResponse(HttpStatus httpStatus, Exception e, WebRequest request) {
+        return ResponseEntity.status(httpStatus)
+                .body(ErrorResponseBody.builder()
+                        .message(e.getMessage())
+                        .description(request.getDescription(false))
+                        .build());
     }
 }
